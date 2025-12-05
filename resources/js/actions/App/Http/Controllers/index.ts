3e19694fbd63fa0af4b428a4854959f199dcd394
Noam Shemesh <noams@hey.com>
2025-12-05 19:32:52 +0200

    🔧 stop tracking Wayfinder generated files
    These files are auto-generated and already in .gitignore

--- resources/js/actions/App/Http/Controllers/index.ts
+++
@@ -1,9 +0,0 @@
-import User from './User'
-import Auth from './Auth'
-
-const Controllers = {
-    User: Object.assign(User, User),
-    Auth: Object.assign(Auth, Auth),
-}
-
-export default Controllers
