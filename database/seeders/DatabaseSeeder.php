3057d387df710f6b4f34848cee1a04687c91b55d
Noam Shemesh <noams@hey.com>
2026-09-17 13:59:33 +0300

    🔧 enforce Model::query() via Larastan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ final class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::firstOrCreate([
+        User::query()->firstOrCreate([
             'email' => 'test@example.com',
         ], [
             'name' => 'Test User',
